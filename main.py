b6f9fc549feafe87ba382f7e6e25e9a824d13327
camell river <cammelriver@gmail.com>
2026-03-27 15:05:09 +0000

    New but bugged

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #Activity 3.2.2 Step 7
-from Post import Post
+from post import Post
 
 # How will you save the posts you will create? Review the for loop 
 # near the end of this code for an answer.
 all_posts_archive = []
 
 # What is the user name they want to use?
-john = input("welcome, what is your username?")
+username = input("welcome, what is your username?")
 
 # Welcome user to the program 
-print("ok", john, "what do you want to do next?")
+print("ok", username, "what do you want to do next?")
 
 # Store initial user input in a variable identified by user_input
 
@@ -28,24 +28,27 @@ user_input = input(""" What would you like to do?
 # statements at the next indentation level until the user types "quit" 
 # in response to the prompt.
 while user_input != "quit":
+    
+    # code for adding a post here
     if user_input == "add":
         message = input("what is the message")
-        post1 = Post(john, message)
+        post1 = Post(username, message)
         all_posts_archive.append(post1)
         print(post1)
-
-    # code for adding a post here
     # code for removing a post here
+    elif user_input == "remove":
+        (all_posts_archive) == ("EMPTY")
     # code for changing the current user here
+    elif user_input == "change user":
+        username = input("Ok, what is your new username?")
+        print(username)
     # code to display all posts, you can use the code in comments below
-    """
-	# this for loop will print the list
-	# and the element itself
-    for post in all_posts_archive: 
-        print (post)
-    """
+    elif user_input == "print":
+        for post in all_posts_archive:
+            print (post)
     # code to inform the user that their input was not valid here
-    
+    else
+        print("please choose a valid option")
     # code that will allow the user to make a new selection
     # This code will finish the loop
     user_input = input(""" What would you like to do?
